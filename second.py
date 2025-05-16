6ebbcfb88763a502b57d498c40504b54e9f2034c
SrutiDash <srutidash1203@gmail.com>
2025-05-16 16:54:51 +0530

    single and multiline comments in python

--- second.py
+++ second.py
@@ -1,1 +1,14 @@
 print("sruti dash")
+
+# this is a python code to print my name
+# single line comment in python we can use as #
+"""
+Python is a versatile, high-level programming language renowned for 
+its readability and simplicity. It is used for a wide range of 
+applications, including web development, data analysis, and machine 
+learning. Python's design emphasizes code clarity and a syntax that's
+often described as similar to plain English, making it easier to 
+learn and use.  
+"""
+# this is called multiline comments in python y using """ this
+
